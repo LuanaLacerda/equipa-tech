c277233e852e66ccdb3a2a88252112d9b1a3bf11
Luana Lacerda <luanaretifica@hotmail.com>
2023-05-28 18:54:08 -0300

    Alteração de rotas e conclusao da rota de doacoes

---
+++ src/controladores/doacoes.js
@@ -0,0 +1,32 @@
+const knex = require('../conexao');
+
+const cadastrarDoacoes = async (req, res) => {
+    const { nome, cpf_cnpj, data_nascimento, telefone, email } = req.body
+
+    if (!nome || !cpf_cnpj || !data_nascimento || !telefone || !email) {
+        return res.status(400).json({ messagem: "Todos os campos são obrigatórios" })
+    };
+
+    try {
+
+        const doacao = await knex('doacoes').insert({
+            nome,
+            cpf_cnpj,
+            data_nascimento,
+            telefone,
+            email
+        })
+
+        if (!doacao) {
+            return res.status(400).json('Usuário não foi cadastrado.')
+        }
+
+        return res.status(201).json('Usuário cadastrado com sucesso.')
+
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
+
+}
+
+module.exports = cadastrarDoacoes
--- src/rotas.js
+++ src/rotas.js
@@ -1,26 +1,24 @@
 const express = require("express");
-const usuarios = require('./controladores/usuarios');
 const rotas = express();
+const doacoes = require('./controladores/doacoes');
+const usuarios = require('./controladores/usuarios');
 const login = require('./controladores/login');
-//const verificaLogin = require('./filtros/verificarLogin');
-
+const verificaLogin = require('./filtros/verificaLogin');
 
 
 //Cadastro usuario
 rotas.post('/usuario', usuarios.cadastrarUsuario);
-
+//Cadastro doação
+rotas.post('/doacao', doacoes);
 //login
 rotas.post('/login', login);
 
 //filtro para veriicar login
-//rotas.use(verificaLogin);
+rotas.use(verificaLogin);
 
 //obter, atualizar usuario
 rotas.get('/usuario', usuarios.listarDadosUsuarioLogado);
 rotas.put('/usuario', usuarios.atualizarUsuario);
-rotas.get('/usuario/:id');
-
-
 
 
 
