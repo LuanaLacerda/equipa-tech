0df85ec939cc7393836972528ed3854ec3ce22a5
Luana Lacerda <luanaretifica@hotmail.com>
2023-05-28 22:51:17 -0300

    teste deploy

--- src/conexao.js
+++ src/conexao.js
@@ -1,12 +1,14 @@
 const knex = require('knex')({
     client: 'pg',
-    host: "postgres://wtpbaznz:L31ezxjvjIWge4Nelnd8hxnidBrJJYIS@silly.db.elephantsql.com/wtpbaznz",
-    user: "wtpbaznz",
-    password: "L31ezxjvjIWge4Nelnd8hxnidBrJJYIS",
-    database: "wtpbaznz",
-    port: "5432",
-    ssl: {
-        rejectUnauthorized: false
+    connection: {
+        host: "postgres://wtpbaznz:L31ezxjvjIWge4Nelnd8hxnidBrJJYIS@silly.db.elephantsql.com/wtpbaznz",
+        user: "wtpbaznz",
+        password: "L31ezxjvjIWge4Nelnd8hxnidBrJJYIS",
+        database: "wtpbaznz",
+        port: "5432",
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 });
 
