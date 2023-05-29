093371c8fe256cc0b39b9f819440c66a635a05fb
Luana Lacerda <luanaretifica@hotmail.com>
2023-05-28 22:08:09 -0300

    teste

--- src/conexao.js
+++ src/conexao.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
+
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: "postgres://wtpbaznz:H9N8nkTyRFIhMw99xP9752T4kXX7faoR@silly.db.elephantsql.com/wtpbaznz",
-        user: "wtpbaznz",
-        password: "H9N8nkTyRFIhMw99xP9752T4kXX7faoR",
-        database: "wtpbaznz",
-        port: "5432"
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        port: process.env.DB_PORT
     }
 });
 
