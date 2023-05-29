84d1ef85c202b77c4943de38f7b57b2e91198dc8
Luana Lacerda <luanaretifica@hotmail.com>
2023-05-28 22:55:35 -0300

    Atualizando o banco de dados para conexao local

--- src/conexao.js
+++ src/conexao.js
@@ -1,15 +1,14 @@
+
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: "postgres://wtpbaznz:L31ezxjvjIWge4Nelnd8hxnidBrJJYIS@silly.db.elephantsql.com/wtpbaznz",
-        user: "wtpbaznz",
-        password: "L31ezxjvjIWge4Nelnd8hxnidBrJJYIS",
-        database: "wtpbaznz",
-        port: "5432",
-        ssl: {
-            rejectUnauthorized: false
-        }
+        host: "localhost",
+        user: "postgres",
+        password: "270808",
+        database: "equipa_tech",
+        port: "5432"
     }
 });
 
 module.exports = knex
+
